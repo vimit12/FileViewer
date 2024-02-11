63a4376e93b6a7f571ae09cbbc75ad3a3d2b1b2b
Vimit <talk2vimit@gmail.com>
2024-02-11 19:21:06 +0530

    main.js adjusted for view

--- frontend-app/src/main.js
+++ frontend-app/src/main.js
@@ -3,13 +3,18 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import '@mdi/font/css/materialdesignicons.min.css';
+
+
+// Vuetify
+import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/lib/components'
-import * as directives from 'vuetify/lib/directives'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 const vuetify = createVuetify({
     components,
-    directives
+    directives,
 })
 
 createApp(App).use(router).use(vuetify).use(VueAxios, axios).mount('#app')
