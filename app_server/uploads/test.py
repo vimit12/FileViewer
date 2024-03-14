eb778c3e6b0afa9f64264641718448de4980da29
Vimit <46067663+vimit12@users.noreply.github.com>
2024-03-14 15:59:14 +0530

    test script

---
+++ app_server/uploads/test.py
@@ -0,0 +1,80 @@
+import json
+import pandas as pd
+
+def main():
+    # Load JSON data
+    with open('sample_output.json') as f:
+        json_data = json.load(f)
+
+    HEADERS = ['Account ID', 'AWS Region', 'Stack Name', 'Drift Status', 'Resource Type', 'Stack Resource Drift Status',
+               'Property Differences', 'Message']
+
+    HEADER_FORMAT = {'bold': True, 'align': 'center', 'valign': 'vcenter', 'bg_color': '#B5D38D', 'border': 1}
+    CELL_FORMAT = {'align': 'center', 'valign': 'vcenter', 'border': 1, 'text_wrap': True}
+    property_counter = 3
+    resource_counter = 3
+    # Create DataFrame
+    df = pd.DataFrame(columns=HEADERS)
+
+    # Write the DataFrame to an Excel file without the extra header row
+    excel_filename = "data.xlsx"
+    with pd.ExcelWriter(excel_filename, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False, sheet_name='Sheet1')
+
+        # Set custom widths for header columns
+        column_widths = [15, 15, 15, 15, 15, 30, 20, 20, 20, 20, 20, 15]  # Adjust widths as needed
+        worksheet = writer.sheets['Sheet1']
+        for i, width in enumerate(column_widths):
+            worksheet.set_column(i, i, width)
+
+        for index, i in enumerate(HEADERS[:6]):
+            cell_merge = f"{chr(65+index)}1:{chr(65+index)}2"
+            worksheet.merge_range(cell_merge, i, writer.book.add_format(HEADER_FORMAT))
+        else:
+            worksheet.merge_range('K1:K2', 'Message', writer.book.add_format(HEADER_FORMAT))
+
+        # Add the extra header row below 'Property Differences' and merge the cell
+        worksheet.merge_range('G1:J1', 'Property Differences', writer.book.add_format(HEADER_FORMAT))
+        extra_headers = ['Property', 'Difference', 'Expected value', 'Actual value']
+        for i, header in enumerate(extra_headers):
+            worksheet.write(1, i + 6, header, writer.book.add_format(HEADER_FORMAT))
+
+        for index, i in enumerate(json_data):
+            json_data_flag = 0
+            start_j_index = property_counter
+            # data for merging and single data
+            for j_index, j in enumerate(i["Resources"]):
+                resource_flag = 0
+                start_index = property_counter
+                # data for merging and merged data data
+                for k_index, k in enumerate(j["PropertyDifferences"]):
+                    resource_flag += 1
+                    cell_id = property_counter
+                    worksheet.write(f"G{cell_id}", k.get("Property Path"), writer.book.add_format(CELL_FORMAT))
+                    worksheet.write(f"H{cell_id}", k.get("Difference Type"), writer.book.add_format(CELL_FORMAT))
+                    worksheet.write(f"I{cell_id}", k.get("Expected value"), writer.book.add_format(CELL_FORMAT))
+                    worksheet.write(f"J{cell_id}", k.get("Actual value"), writer.book.add_format(CELL_FORMAT))
+                    property_counter += 1
+                else:
+                    end_index = cell_id
+                    e_cell = f'E{start_index}:E{end_index}'
+                    f_cell = f'F{start_index}:F{end_index}'
+                    worksheet.merge_range(e_cell, j.get("ResourceType"),
+                                          writer.book.add_format(CELL_FORMAT))
+                    worksheet.merge_range(f_cell, j.get("StackResourceDriftStatus"),
+                                          writer.book.add_format(CELL_FORMAT))
+            else:
+                enf_j_index = cell_id
+                a_cell = f'A{start_j_index}:A{enf_j_index}'
+                b_cell = f'B{start_j_index}:B{enf_j_index}'
+                c_cell = f'C{start_j_index}:C{enf_j_index}'
+                d_cell = f'D{start_j_index}:D{enf_j_index}'
+                worksheet.merge_range(a_cell, i.get("Account ID"), writer.book.add_format(CELL_FORMAT))
+                worksheet.merge_range(b_cell, i.get("AWS Region"), writer.book.add_format(CELL_FORMAT))
+                worksheet.merge_range(c_cell, i.get("Stack Name"), writer.book.add_format(CELL_FORMAT))
+                worksheet.merge_range(d_cell, i.get("Drift Status"), writer.book.add_format(CELL_FORMAT))
+
+    print("SUCCESS")
+
+if __name__ == '__main__':
+    main()
